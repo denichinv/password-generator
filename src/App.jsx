44d21308adb6c1ebe93aed4ee69840fd75042fff
Vilizar Denichin <denichinv@gmail.com>
2025-01-18 17:15:05 +0000

    checkboxes ,button and field implemented

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     if (charAllow) strData + "!@#$%^&*=-_`~+:;<.>?/|,"
     for (let index = 0; index < len; index++) {
       let position = Math.floor(Math.random() * strData.length + 1)
-      pass = strData.charAt(position)
+      pass += strData.charAt(position)
       
     }
     setPassword(pass)
@@ -29,6 +29,23 @@ function App() {
       <h1>Password Generator App</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input type="text" value={pass} className='outline-none w-full py-1 px-3' placeholder='password' readOnly/>
+        <button className='ml-2' onClick={passwordGenerator}>Generate</button>
+      </div>
+      <div className='flex text-sm gap-x-2 '>
+        <div className='flex items-center gap-x-1'>
+          <input type="range" min={6} max={100} value={len} className='cursor-pointer' onChange={(e) => {setLen(e.target.value)}}  />
+          <label>Length: {len}</label>
+        </div>
+        <div className='flex text-sm gap-x-2 '>
+          <input type="checkbox" defaultChecked={numAllow}
+          id='numbInput'
+          onChange={() => {setNumAllow((prev)=> !prev)}}/>
+          <label htmlFor="numbInput">Numbers</label>
+          <input type="checkbox" defaultChecked={charAllow}
+          id='charInput'
+          onChange={() => {setCharAllow((prev)=> !prev)}}/>
+          <label htmlFor="charInput">Characters</label>
+        </div>
       </div>
       </div>
     </>
